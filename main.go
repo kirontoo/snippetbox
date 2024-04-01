1a6a176347cf1a2991d0bb9b3a2813e1b1f6576e
Amy Dang <kirontoo@gmail.com>
2024-04-01 14:25:10 -0700

    set up web server and routes

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// Home handler function
+func home(w http.ResponseWriter, r *http.Request) {
+  // if the current URL path does not exactly match "/" then it is not the home page
+  // return a 404
+  if r.URL.Path != "/" {
+    http.NotFound(w, r)
+    return
+  }
+
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add a snippetView handler function.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
 
 func main() {
-  fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+
+	// Register handler functions
+	// this is where all of your routes (URL patterns) will go
+	mux.HandleFunc("/", home)
+  mux.HandleFunc("/snippet/view", snippetView)
+  mux.HandleFunc("/snippet/create", snippetCreate)
+
+	// Start server on TCP network address ":4000"
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
