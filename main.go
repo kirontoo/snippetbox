7d6627bfb8a774aabf512d19d9327084b453cfd7
Amy Dang <kirontoo@gmail.com>
2024-04-01 15:13:02 -0700

    grab query "id" and validate

--- main.go
+++ main.go
@@ -1,38 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Home handler function
 func home(w http.ResponseWriter, r *http.Request) {
-  // if the current URL path does not exactly match "/" then it is not the home page
-  // return a 404
-  if r.URL.Path != "/" {
-    http.NotFound(w, r)
-    return
-  }
+	// if the current URL path does not exactly match "/" then it is not the home page
+	// return a 404
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
 // Add a snippetView handler function.
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-  if r.Method != http.MethodPost {
-    // If it's not a POST method, send status code 405 and meesage 
-    // "Method Not Allowed"
-    w.Header().Set("Allow", "POST")
-    http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-    return
-  }
+	if r.Method != http.MethodPost {
+		// If it's not a POST method, send status code 405 and meesage
+		// "Method Not Allowed"
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
 	w.Write([]byte("Create a new snippet..."))
+}
+
+func setJSONHeader(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
 }
 
 func main() {
@@ -41,8 +53,8 @@ func main() {
 	// Register handler functions
 	// this is where all of your routes (URL patterns) will go
 	mux.HandleFunc("/", home)
-  mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Start server on TCP network address ":4000"
 	log.Print("Starting server on :4000")
