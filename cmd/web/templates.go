76d409255185cd14567e207d8176b40878558321
Amy Dang <kirontoo@gmail.com>
2024-04-10 13:27:35 -0700

    refactor to load all partials

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"text/template"
 	"path/filepath"
+	"text/template"
 
 	"github.com/kirontoo/snippetbox/internal/models"
 )
@@ -16,26 +16,31 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
 	// get a slice of all filepaths that match the pattern 
-	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
 	if err != nil {
 		return nil, err
 	}
 	for _, page := range pages {
 		name := filepath.Base(page)
-		files := []string{
-			"./ui/html/base.tmpl.html",
-			"./ui/html/partials/nav.tmpl.html",
-			page,
+
+		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
+		if err != nil {
+			return nil, err
 		}
 
 		// parse files into a template set
-		ts, err := template.ParseFiles(files...)
+		ts, err = ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
 
 		// add template set to the map, using the name of the page as the key
-		// (like 'home.tmpl') as the key.
+		// (like 'home.tmpl.html') as the key.
 		cache[name] = ts
 	}
 
