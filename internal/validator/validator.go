e1de364a7a95b31547e218580b7bac504de358a8
Amy Dang <kirontoo@gmail.com>
2024-04-17 13:10:42 -0700

    add validators: email, matches, and minChars

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,6 +1,7 @@
 package validator
 
 import (
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
@@ -8,11 +9,11 @@ import (
 // form fields.
 type Validator struct {
 	FieldErrors map[string]string
-} 
+}
 
 func (v *Validator) Valid() bool {
 	return len(v.FieldErrors) == 0
-} 
+}
 
 func (v *Validator) AddFieldError(key, message string) {
 	if v.FieldErrors == nil {
@@ -31,7 +32,8 @@ func (v *Validator) CheckField(ok bool, key, message string) {
 
 func NotBlank(value string) bool {
 	return strings.TrimSpace(value) != ""
-} 
+}
+
 func MaxChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) <= n
 }
@@ -43,4 +45,14 @@ func PermittedInt(value int, permittedValues ...int) bool {
 		}
 	}
 	return false
+}
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
 }
