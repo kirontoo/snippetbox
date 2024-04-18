4c951ff66312bade5586bd464c43d2f53b45e79b
Amy Dang <kirontoo@gmail.com>
2024-04-18 12:27:47 -0700

    redirct user to login if not authorized

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -38,3 +38,16 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAuthenticated(r) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+
+		// require authentication is not stored in the users browser cache
+		w.Header().Add("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
+}
