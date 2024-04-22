e267edafa2a82d32a11bee5697eb1feed111021b
Amy Dang <kirontoo@gmail.com>
2024-04-22 12:03:24 -0700

    embed template files into binary

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"io/fs"
 	"path/filepath"
 	"text/template"
 	"time"
 
 	"github.com/kirontoo/snippetbox/internal/models"
+	"github.com/kirontoo/snippetbox/ui"
 )
 
 type templateData struct {
@@ -30,28 +32,24 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
 	// get a slice of all filepaths that match the pattern
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl.html")
 	if err != nil {
 		return nil, err
 	}
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl.html")
+		patterns := []string {
+			"html/base.tmpl.html",
+			"html/partials/*.tmpl.html",
+			page,
+		}
+
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
 
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
-		if err != nil {
-			return nil, err
-		}
-
-		// parse files into a template set
-		ts, err = ts.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
 
 		// add template set to the map, using the name of the page as the key
 		// (like 'home.tmpl.html') as the key.
